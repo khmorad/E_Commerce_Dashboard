80c8b5f3b05dc8ce27b4a4e8438a7e632a1ca122
khmoorad <khakho.morad@gmail.com>
2023-11-22 11:26:52 -0800

    added the api + more ui implementation

--- build/data.py
+++ build/data.py
@@ -1,44 +1,66 @@
+
+
 import pandas as pd
+import requests
+#"207c3389f3msh11486cf94cb0b1dp16574bjsn187424d96ce9"  //key not working --reached monthly limit :(--
 
-data = {
-    'id': list(range(1, 11)),
-    'seller_name': [
-        'BOOK INDUSTRIES', 'Walmart.com', 'JIYA EXPORTS', 'Book Outlet USA', 'SHOPPINGSS',
-        'JIYA EXPORTS', 'Walmart.com', 'Walmart.com', 'BOOK INDUSTRIES', 'JIYA EXPORTS'
-    ],
-    'item_price': [
-        19, 7, 9, 6, 10, 10, 10, 8, 10, 10
-    ],
-    'Item_line_price': [
-        '$19.99', '$7.49', '$9.10', '$6.99', '$10.50', '$10.15', '$10.20', '$8.97', '$10.99', '$10.55'
-    ],
-    'availability': [
-        True, True, True, True, True, True, True, True, True, True
-    ],
-    'avg_rating': [
-        5, 4, 4, 4, 4, 4, 4, 4, 4, 4
-    ],
-    'reviews': [
-        16, 50, 115, 94, 30, 14, 28, 5, 11, 186
-    ],
-    'item_name': [
-        'The Wrinkle in Time Boxed Set, Includes 5 Book...',
-        'Girl in Pieces (Paperback)',
-        'It Starts with Us : A Novel (Paperback)',
-        'Verity (Paperback)',
-        'Twisted: Twisted Love (Paperback)',
-        'Things We Never Got Over (Knockemout)Paperback...',
-        'Never After: Hooked (Paperback)',
-        'The Maple Hills: Icebreaker (Paperback)',
-        'Shatter Me: Shatter Me Series 6-Book Box Set: ...',
-        'Dreamland Billionaires: The Fine Print (Series...'
-    ]
-}
 
-# Create DataFrame from the provided data
-df = pd.DataFrame(data)
+def fetch_walmart_data_by_keyword(keyword):
+    url = "https://axesso-walmart-data-service.p.rapidapi.com/wlm/walmart-search-by-keyword"
+    headers = {
+        "X-RapidAPI-Key": "dcd7cbe2demsh2762fda9960c05cp1fcc84jsnb21bf36130d9",
+        "X-RapidAPI-Host": "axesso-walmart-data-service.p.rapidapi.com"
+    }
+    querystring = {
+        "keyword": keyword,
+        "page": "1",
+        "sortBy": "best_match"
+    }
 
-# Set 'id' as the index of the DataFrame
-df.set_index('id', inplace=True)
+    response = requests.get(url, headers=headers, params=querystring)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+def extract_walmart_data(json_data):
+    data = json_data['item']['props']['pageProps']['initialData']['searchResult']['itemStacks']
+    item_data = {
+        'id': [],
+        'seller_name': [],
+        'item_price': [],
+        'Item_line_price': [],
+        'availability': [],
+        'avg_rating': [],
+        'reviews': [],
+        'item_name': []
+    }
+
+    for stack in data:
+        for item in stack['items']:
+            try:
+                item_data['id'].append(item['usItemId'])
+                item_data['seller_name'].append(item['sellerName'])
+                item_data['item_price'].append(item['price'])
+                item_data['Item_line_price'].append(item['priceInfo']['linePrice'])
+                item_data['availability'].append(not item['isOutOfStock'])
+                item_data['avg_rating'].append(item['rating']['averageRating'])
+                item_data['reviews'].append(item['rating']['numberOfReviews'])
+                item_data['item_name'].append(item['name'])
+            except KeyError:
+                print("Some data is missing")
+    return pd.DataFrame(item_data)
+
+def process_walmart_data_by_keyword(keyword):
+    walmart_json_data = fetch_walmart_data_by_keyword(keyword)
+    if walmart_json_data:
+        walmart_df = extract_walmart_data(walmart_json_data)
+        walmart_df.to_csv('walmart_data.csv', index=False)
+        return walmart_df
+    else:
+        return None
+
+
 
 
